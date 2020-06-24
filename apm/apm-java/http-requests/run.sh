b5e582898da0a141eb05aa5c1209c366c1ee5d13
Steve L <slerner@splunk.com>
2020-06-24 08:45:05 -0400

    Update run.sh

--- apm/apm-java/http-requests/run.sh
+++ apm/apm-java/http-requests/run.sh
@@ -5,4 +5,5 @@ mvn compile exec:exec \
   -Dexec.args="-Dcom.sun.management.jmxremote.port=3000 \
   -Dcom.sun.management.jmxremote.authenticate=false \
   -Dcom.sun.management.jmxremote.ssl=false \
+  -Dcom.sun.management.jmxremote.rmi.port=3000 \
   -javaagent:signalfx-tracing.jar -cp %classpath sf.main.GetExample"
